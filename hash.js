33e1229898010b58a0689ae3c1d7970b153a9d29
w453908766 <w453908766@126.com>
2024-09-26 11:58:37 +0800

    use flexible prefix

--- hash.js
+++ hash.js
@@ -3,5 +3,11 @@ let md5 = require('js-md5')
 
 export function convert(text) {
   let hash = md5.base64(text)
-  return "Aa0=" + hash.substr(0, 8)
+  let arr = md5.array(text)
+  let A = "A".charCodeAt(0) + arr[12] % 26
+  let a = "a".charCodeAt(0) + arr[13] % 26
+  let num = "0".charCodeAt(0) + arr[14] % 10
+  let sym = arr[15] % 2 ? '/' : '+'
+
+  return String.fromCharCode(A, a, num) + sym + hash.substr(0, 8)
 }
